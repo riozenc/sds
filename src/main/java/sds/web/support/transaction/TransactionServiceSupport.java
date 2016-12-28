d8e25a904a2d18e8539719fd49066712297fc83c
riozenc <riozenc@DESKTOP-US6TTP8>
2016-12-28 14:36:43 +0800

    基础类设置不可继承

--- src/main/java/sds/web/support/transaction/TransactionServiceSupport.java
+++ src/main/java/sds/web/support/transaction/TransactionServiceSupport.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Configuration;
 import com.riozenc.quicktool.springmvc.transaction.registry.TransactionServiceRegistryPostProcessor;
 
 @Configuration
-public class TransactionServiceSupport extends TransactionServiceRegistryPostProcessor {
+public final class TransactionServiceSupport extends TransactionServiceRegistryPostProcessor {
 
 }
