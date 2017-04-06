c87f9555d8b721e6b1382d3ee7a5885e3c7324f2
luan <luan@luan-PC>
2017-04-06 16:29:51 +0800

    增加手机判断

--- src/main/java/sds/webapp/sys/action/LoginAction.java
+++ src/main/java/sds/webapp/sys/action/LoginAction.java
@@ -43,8 +43,11 @@ public class LoginAction {
 				// 非法请求
 				return loginFail("IncorrectCredentialsException", httpServletRequest, httpServletResponse);
 			}
-			//增加session
+			//增加session 还需要考虑
+			boolean mobile = WebUtils.isTrue(httpServletRequest, PasswordShiroFilter.DEFAULT_MOBILE_PARAM);
+			if(!mobile){
 			httpServletRequest.getSession().setAttribute("username",principal.getUserDomain().getAccount());
+			}
 			return JSONUtil.toJsonString(new JsonResult(JsonResult.SUCCESS, principal.getUserName()));
 
 		} else {
@@ -72,7 +75,7 @@ public class LoginAction {
 		boolean mobile = WebUtils.isTrue(request, PasswordShiroFilter.DEFAULT_MOBILE_PARAM);
 		String exception = (String) request.getAttribute(PasswordShiroFilter.DEFAULT_ERROR_KEY_ATTRIBUTE_NAME);
 		String message = (String) request.getAttribute(PasswordShiroFilter.DEFAULT_MESSAGE_PARAM);
-
+		
 		if (StringUtils.isBlank(message)) {
 			message = "用户或密码错误, 请重试.";
 		}
