47ea99d8e7acc9b8cf3b7b109245f19e4125a1b8
luan <luan@luan-PC>
2017-04-26 17:28:42 +0800

    增加非空值判断

--- src/main/java/sds/web/filter/RegisterCorsFilter.java
+++ src/main/java/sds/web/filter/RegisterCorsFilter.java
@@ -37,10 +37,12 @@ public class RegisterCorsFilter implements Filter {
 		HttpServletRequest servletRequest = (HttpServletRequest) request;
 		HttpServletResponse servletResponse = (HttpServletResponse) response;
 		String path = servletRequest.getQueryString();
-		if(path.indexOf("getRegisterVerificationCode")!=-1||path.indexOf("register")!=-1){
+		if(path!=null){
+			if(path.indexOf("getRegisterVerificationCode")!=-1||path.indexOf("register")!=-1){
 			servletResponse.addHeader("Access-Control-Allow-Origin", "*");
 			servletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET");
 			servletResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With, Origin");	
+			}
 		}
 		chain.doFilter(request, response);
 	}
