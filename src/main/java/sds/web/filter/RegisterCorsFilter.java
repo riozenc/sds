ce575c997d090cb816ce2b58b6a874fd772fbbfc
luan <luan@luan-PC>
2017-04-26 16:39:25 +0800

    配置getRegisterVerificationCode、registerMerchant 两个方法的拦截器

--- src/main/java/sds/web/filter/RegisterCorsFilter.java
+++ src/main/java/sds/web/filter/RegisterCorsFilter.java
@@ -9,32 +9,43 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class RegisterCorsFilter implements Filter {
-
+	
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		Filter.super.destroy();
+	}
+	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		
-		
-		// getRegisterVerificationCode
-		// registerMerchant
-		
-		
-		
-		
-		
-//		if(){
-//			httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
-//			httpServletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET");
-//			httpServletResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With, Origin");	
-//		}
-		
-		chain.doFilter(request, response);
+		// getRegisterVerificationCode  请求时走这个方法
+		// registerMerchant 请求时走这个方法
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		String path = servletRequest.getQueryString();
+		//System.out.println("路径信息"+path);
+		if(path.indexOf("getRegisterVerificationCode")!=-1||path.indexOf("register")!=-1){
+			servletResponse.addHeader("Access-Control-Allow-Origin", "*");
+			servletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET");
+			servletResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Requested-With, Origin");	
+		}
+		System.out.println(servletResponse.getHeader("Access-Control-Allow-Methods"));
+		chain.doFilter(servletRequest, servletResponse);
 	}
-
 }
+
+
